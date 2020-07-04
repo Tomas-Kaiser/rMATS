7609413610433de5764caabfc6410635ad027ef8
Tomas Kaiser <tomas.kaiser06@gmail.com>
2020-07-04 11:48:27 +0100

    Initial commit

---
+++ src/main/java/com/schoolproject/rmats/RmatsApplication.java
@@ -0,0 +1,13 @@
+package com.schoolproject.rmats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RmatsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RmatsApplication.class, args);
+    }
+
+}
