f93591c48b1a0ec2f2ffeea70459e31891bd3524
Tomas Kaiser <tomas.kaiser06@gmail.com>
2020-07-27 19:27:18 +0100

    Added FaultyRT

---
+++ src/main/java/com/schoolproject/rmats/api/rt/FaultyRT.java
@@ -0,0 +1,31 @@
+package com.schoolproject.rmats.api.rt;
+
+public class FaultyRT {
+    private Integer id;
+    private String model;
+    private String serialNumber;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+}
