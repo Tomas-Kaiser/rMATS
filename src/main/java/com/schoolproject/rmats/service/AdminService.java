9a0a129361524127747930674d8ddf76ff0d8006
Tomas Kaiser <tomas.kaiser06@gmail.com>
2020-08-03 15:44:27 +0100

    Refactoring updateReplacementUnit method

--- src/main/java/com/schoolproject/rmats/service/AdminService.java
+++ src/main/java/com/schoolproject/rmats/service/AdminService.java
@@ -18,21 +18,27 @@ public class AdminService {
         this.ticketRepository = ticketRepository;
     }
 
+    // Create a repalcement unit
     @Transactional
     public void createReplacementUnit(ReplacementUnit replacementUnit){
         replacementRepository.save(replacementUnit);
     }
 
+    // Update a replacement unit
     @Transactional
     public void updateReplacementUnit(ReplacementUnit replacementUnitUpdate, int replacementId){
-
         ReplacementUnit replacementUnit = replacementRepository.findById(replacementId);
+        replacementUnit = createReplacementUnit(replacementUnitUpdate, replacementUnit);
+        replacementRepository.save(replacementUnit);
+    }
+
+    private ReplacementUnit createReplacementUnit(ReplacementUnit replacementUnitUpdate, ReplacementUnit replacementUnit){
         replacementUnit.setComment(replacementUnitUpdate.getComment());
         replacementUnit.setNewSerialNumber(replacementUnitUpdate.getNewSerialNumber());
         replacementUnit.setModel(replacementUnitUpdate.getModel());
         replacementUnit.setCarrier(replacementUnitUpdate.getCarrier());
         replacementUnit.setStatus(replacementUnitUpdate.getStatus());
         replacementUnit.setTrackingNumber(replacementUnitUpdate.getTrackingNumber());
-        replacementRepository.save(replacementUnit);
+        return  replacementUnit;
     }
 }
